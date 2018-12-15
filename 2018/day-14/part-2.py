d72504458dd814c5fc221456f888ec8b12653d76
Dillon Dumesnil <dillon.dumesnil@gmail.com>
2018-12-15 14:08:36 -0500

    2018 day 14

--- 2018/day-14/part-2.py
+++ 2018/day-14/part-2.py
@@ -14,58 +14,64 @@ class Elf(object):
         return 'Index: ' + str(self.current_recipe_index) + ', Score: ' + str(self.current_recipe_score)
 
 
-def make_recipes(scoreboard, num_recipes_to_go_through):
+def make_recipes(scoreboard, sequence_to_find):
     elves = [Elf(0, scoreboard[0]), Elf(1, scoreboard[1])]
 
-    ten_recipes = ''
-    while len(ten_recipes) < 10:
+    num_recipes = len(scoreboard)
+    recipe_string = ''.join([str(recipe) for recipe in scoreboard])
+    for i in range(len(sequence_to_find) - len(scoreboard)):
+        recipe_string = '.' + recipe_string
+
+    while sequence_to_find != recipe_string:
         new_recipe = sum([elf.current_recipe_score for elf in elves])
         if new_recipe // 10:
             scoreboard.append(1)
-            if len(scoreboard) > num_recipes_to_go_through:
-                ten_recipes += '1'
+            recipe_string = recipe_string[1:] + '1'
+            num_recipes += 1
+        if sequence_to_find == recipe_string:
+            break
         scoreboard.append(new_recipe % 10)
-        if len(scoreboard) > num_recipes_to_go_through and len(ten_recipes) < 10:
-            ten_recipes += str(new_recipe % 10)
+        recipe_string = recipe_string[1:] + str(new_recipe % 10)
+        num_recipes += 1
         for elf in elves:
             elf.new_current_recipe(scoreboard)
 
-    return ten_recipes
+    return num_recipes - len(sequence_to_find)
 
 
 if __name__ == '__main__':
     initial_scoreboard = [3, 7]
-    INPUT = 760221
-    ten_recipes = make_recipes(initial_scoreboard, INPUT)
-    print(ten_recipes)
+    INPUT = '760221'
+    num_recipes = make_recipes(initial_scoreboard, INPUT)
+    print(num_recipes)
 
     # initial_scoreboard = [3, 7]
-    # test_input_1 = 9
-    # expected_output_1 = '5158916779'
+    # test_input_1 = '51589'
+    # expected_output_1 = 9
     # actual_output_1 = make_recipes(initial_scoreboard, test_input_1)
     # print('Test # 1:')
     # print('expected_output:', expected_output_1)
     # print('actual_output:', actual_output_1)
 
     # initial_scoreboard = [3, 7]
-    # test_input_2 = 5
-    # expected_output_2 = '0124515891'
+    # test_input_2 = '01245'
+    # expected_output_2 = 5
     # actual_output_2 = make_recipes(initial_scoreboard, test_input_2)
     # print('Test # 2:')
     # print('expected_output:', expected_output_2)
     # print('actual_output:', actual_output_2)
 
     # initial_scoreboard = [3, 7]
-    # test_input_3 = 18
-    # expected_output_3 = '9251071085'
+    # test_input_3 = '92510'
+    # expected_output_3 = 18
     # actual_output_3 = make_recipes(initial_scoreboard, test_input_3)
     # print('Test # 3:')
     # print('expected_output:', expected_output_3)
     # print('actual_output:', actual_output_3)
 
     # initial_scoreboard = [3, 7]
-    # test_input_4 = 2018
-    # expected_output_4 = '5941429882'
+    # test_input_4 = '59414'
+    # expected_output_4 = 2018
     # actual_output_4 = make_recipes(initial_scoreboard, test_input_4)
     # print('Test # 4:')
     # print('expected_output:', expected_output_4)
